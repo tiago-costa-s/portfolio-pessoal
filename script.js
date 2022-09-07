ed5769113b433f38ae6385e34a71b392af15b0fa
“tGUhh“ <tiagocostasantos0013@gmail.com>
2022-09-07 00:31:04 -0300

    Ajuste da Sessão 4, formulario, responsividade para Smartphones

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 // Esconder NavBar
-function esconderMenu(){
+function esconderMenu() {
 
-   let cabeca = document.getElementById("top_header");
+    let cabeca = document.getElementById("top_header");
     cabeca.setAttribute = "display = none";
 }
 
@@ -11,7 +11,43 @@ function esconderMenu(){
 var cabeca = document.getElementById("top_header");
 var queryHeader = document.querySelector('#top_header');
 
-window.addEventListener("scroll", function(){
-    var header = document.querySelector("header"); 
+window.addEventListener("scroll", function () {
+    var header = document.querySelector("header");
     header.classList.toggle("top_header", window.scrollY > 0);
-})
+});
+
+
+
+// CAPTURAR DADOS DO FORMULARIO DE CONTATO
+
+
+
+const enviar = document.querySelector("#btn");
+
+enviar.addEventListener("click", function (e) {
+
+    // e.preventDefault();
+
+    const nomeValor = nome.value;
+    const telefoneValor = telefone.value;
+    const descricaoValor = descricao.value;
+
+    const novoContato = { nomeValor, telefoneValor, descricaoValor }
+
+    console.log(nomeValor, telefoneValor, descricaoValor);
+    console.log(novoContato);
+
+
+});
+
+
+function alertaLogin() { 
+
+    try {
+        console.log("");
+    } catch (error) {
+        
+    }
+}
+
+
