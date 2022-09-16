1e47373833ebb4ba58b16c51d66064d34c11e901
“tGUhh“ <tiagocostasantos0013@gmail.com>
2022-09-16 14:41:40 -0300

    Realizado, ajustes de codigo para ficar mais legivel. Efetuado criado função opções ao clickar no Burger Menu

--- script.js
+++ script.js
@@ -2,27 +2,17 @@
 // Esconder NavBar
 function esconderMenu() {
 
-    let cabeca = document.getElementById("top_header");
+    let cabeca = document.getElementById("header");
     cabeca.setAttribute = "display = none";
+
+    // cabeca.style.backgroundColor="red";
 }
 
 // Alterar fundo da NavBar ao realizar o Scroll
 
-var cabeca = document.getElementById("top_header");
-var queryHeader = document.querySelector('#top_header');
-
-window.addEventListener("scroll", function () {
-    var header = document.querySelector("header");
-    header.classList.toggle("top_header", window.scrollY > 0);
-    
-});
-
 
 
 // CAPTURAR DADOS DO FORMULARIO DE CONTATO
-
-
-
 const enviar = document.querySelector("#btn");
 
 enviar.addEventListener("click", function (e) {
@@ -34,9 +24,7 @@ enviar.addEventListener("click", function (e) {
     const descricaoValor = descricao.value;
 
     const novoContato = { nomeValor, telefoneValor, descricaoValor }
-
-    console.log(nomeValor, telefoneValor, descricaoValor);
-    console.log(novoContato);
+  
 
 });
 
@@ -50,4 +38,12 @@ function alertaLogin() {
     }
 }
 
+// Exibi opções do menu ao clicar no burgerMenu
+const burgerMenu = document.getElementById("burger_menu");
 
+function toggleMenu(){
+    const nav = document.getElementById("nav");
+    nav.classList.toggle("active");
+}
+
+burgerMenu.addEventListener("click", toggleMenu);
